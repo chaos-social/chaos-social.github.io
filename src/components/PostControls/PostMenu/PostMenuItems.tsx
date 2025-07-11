a9cf189e66a2bbd0d857c58684b91341daf2efc5
uwx <maxine@cybergal.com>
2025-07-11 18:25:06 +0200

    View bridged post on fedi

--- src/components/PostControls/PostMenu/PostMenuItems.tsx
+++ src/components/PostControls/PostMenu/PostMenuItems.tsx
@@ -768,6 +768,20 @@ let PostMenuItems = ({
             </Menu.Group>
           </>
         )}
+        
+        {(post as { bridgyOriginalUrl?: string }).bridgyOriginalUrl && ( 
+          <>
+            <Menu.Divider />
+            <Menu.Group>
+              <Menu.Item
+                testID="postDropdownBlockBtn"
+                label={_(msg`View on Fediverse`)}
+                onPress={() => document.location.href = (post as unknown as { bridgyOriginalUrl: string }).bridgyOriginalUrl}>
+                <Menu.ItemText>{_(msg`View on Fediverse`)}</Menu.ItemText>
+              </Menu.Item>
+            </Menu.Group>
+          </>
+        )}
       </Menu.Outer>
 
       <Prompt.Basic
